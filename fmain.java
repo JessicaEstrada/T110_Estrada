da4b8888628ee9de6ac0d7c5a5a78d3a88278c0d
JessicaEstrada <73824375+JessicaEstrada@users.noreply.github.com>
2021-09-24 07:47:30 +0700

    Add files via upload

---
+++ fmain.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class fmain {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner (System.in);
+		
+		System.out.print("Enter time in minutes : ");
+		int min = scan.nextInt();
+		
+		int revolutionTime = min * 3000;
+		System.out.println("There are " + revolutionTime +" revolution per minute.");
+		
+		int sec = revolutionTime / 60;
+		System.out.println("\nIt rotates " + sec + " degrees in one second" );
+
+	}
+
+}
